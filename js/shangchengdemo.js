9a0dcf3e4dfd8da992bbf8d608f11f341ef81344
fengyy51 <694590074@qq.com>
2017-04-07 14:43:24 +0800

    demo以及首页增加水平滚动图片效果

--- js/shangchengdemo.js
+++ js/shangchengdemo.js
@@ -1,15 +1,15 @@
-$(document).ready(function() {
+jQuery(document).ready(function() {
     // document.title="title";
 
     loadJson();
     // setInterval(function(){
     // 	loadJson();
     // },1000);
-
+ 
 });
 
 function loadJson() {
-    $.ajax({
+    jQuery.ajax({
         url: "../json/shangchengdemo.json",
         type: "GET",
         dataType: "json",
@@ -24,15 +24,15 @@ function loadJson() {
             var youhui = data.data.youhui;
             // makeTitle(title);
             makeHuoDong(huodong, huodong.length);
-            $('.huodong>.num>span').eq(0).addClass('active');
+            jQuery('.huodong>.num>span').eq(0).addClass('active');
             makeGongGao(gonggao);
             makeShangPin(shangpin);
-            console.log(mingxingdianpu);
+            // console.log(mingxingdianpu);
             makeMingXingDianPu(mingxingdianpu);
             makeYouHuiShangPin(youhuishangpin);
 
-            $('div.youhuiquan .body').empty();
-            $.each(youhui, function(i) {
+            jQuery('div.youhuiquan .body').empty();
+            jQuery.each(youhui, function(i) {
                 makeYouHui(this.tupian, this.text, this.num);
             });
 
@@ -50,14 +50,14 @@ function loadJson() {
 function makeTitle(pageTitle) {
     // var strHtml='<span class="title">'+pageTitle+'</span>';
     var strHtml = pageTitle;
-    $(document).attr('title', strHtml);
+    jQuery(document).attr('title', strHtml);
     // console.log(strHtml);
-    // $('head title').empty();
-    // $('head title').html(strHtml);
+    // jQuery('head title').empty();
+    // jQuery('head title').html(strHtml);
 };
 
 function makeHuoDong(huodong, length) {
-    $('div.guanggao').empty();
+    jQuery('div.guanggao').empty();
     var strHtml = "";
     for (var i = 0; i < length; i++) {
         strHtml = strHtml + '<li class="row"><img src=' + huodong[i] + '></li>';
@@ -69,7 +69,7 @@ function makeHuoDong(huodong, length) {
         // console.log(i);
     }
     strHtml = '<div class="huodong" ><ul class="tupian">' + strHtml + '</div>' + '</div>';
-    $('div.guanggao').html(strHtml);
+    jQuery('div.guanggao').html(strHtml);
     actionHuoDong();
 
 }
@@ -78,17 +78,17 @@ function makeGongGao(gonggao) {
     var flagGongGao = false;
     var i = 0;
     var strHtml = '<span class="text"><span class="red">' + gonggao[0].decoration + '  </span><span class="content">' + gonggao[0].text + '</span></span>' + '';
-    $('div.gonggao ').append(strHtml);
+    jQuery('div.gonggao ').append(strHtml);
     setInterval(function() {
         if (i == gonggao.length)
             i = 0;
 
-        // $('div.gonggao span.red').next().empty();
+        // jQuery('div.gonggao span.red').next().empty();
         var strHtml = '<span class="text"><span class="red">' + gonggao[i].decoration + '</span><span class="content">' + gonggao[i].text + '</span></span>' + '';
-        $('div.gonggao span.text').empty();
+        jQuery('div.gonggao span.text').empty();
         // console.log(strHtml);
         i++;
-        $('div.gonggao ').append(strHtml);
+        jQuery('div.gonggao ').append(strHtml);
 
     }, 4000);
 
@@ -96,7 +96,7 @@ function makeGongGao(gonggao) {
 
 function makeShangPin(shangpin) {
     var strHtml = '<div class="twocolom">';
-    $.each(shangpin, function(i) {
+    jQuery.each(shangpin, function(i) {
         if (i % 2 == 0) {
             strHtml = strHtml + '<div class="colom"><div class="colomitem">' + '<div class="tupian"><img src=' + this.tupian + '></div><div class="content"><p class="decoration">' +
                 this.decoration + '</p><p class="money">' + this.money + '</p></div></div>';
@@ -105,24 +105,57 @@ function makeShangPin(shangpin) {
                 this.decoration + '</p><p class="money">' + this.money + '</p></div></div></div>';
         }
     });
-    $('div.shangpin .body').html(strHtml);
+    jQuery('div.shangpin .body').html(strHtml);
 }
 
 function makeMingXingDianPu(mingxingdianpu) {
-    var optionHtml = '<div class="container">';
-    $.each(mingxingdianpu, function(i) {
-        optionHtml = optionHtml + '<div class="row "><div class="tupian"><img src=' + this.tupian + '></div><div class="content"><p class="decoration">' +
-            this.text + '</p><p class="num">' + this.num + '</p></div></div>';
-    })
-    optionHtml = optionHtml + '</div>';
-    console.log(optionHtml);
-    $('div.mingxingdianpu .body').html(optionHtml);
-    actionMingXingDianPu();
+
+    // var optionHtml = '<div class="container">';
+    // jQuery.each(mingxingdianpu, function(i) {
+    //     optionHtml = optionHtml + '<div class="row "><div class="tupian"><img src=' + this.tupian + '></div><div class="content"><p class="decoration">' +
+    //         this.text + '</p><p class="num">' + this.num + '</p></div></div>';
+    // })
+    // optionHtml = optionHtml + '</div>';
+    // console.log(optionHtml);
+    // jQuery('div.mingxingdianpu .body').html(optionHtml);
+    // var data=new Array();
+    //  //valueMap初始化
+    // for(var k=0;k<9;k++){
+    // data[k]=new Array();
+    // for(var j=0;j<5;j++){
+    //      data[k][j]="";         
+    //      }
+    // }
+    // valueMap[0][0]="pinghezhi";valueMap[1][0]="qixuzhi";valueMap[2][0]="telinzhi";
+    // valueMap[3][0]="xueyuzhi";valueMap[4][0]="yangxuzhi";valueMap[5][0]="yinxuzhi";
+    // valueMap[6][0]="tanshizhi";valueMap[7][0]="shirezhi";valueMap[8][0]="qiyuzhi";
+    var data=new Array();
+    for (var i=0;i<6;i++){
+        data[i]=new Object();
+    }
+    
+    // dat["href","pic","title"];
+        // console.log(data);
+    for(var i=0;i<mingxingdianpu.length;i++){
+        data[i].href="#";
+        data[i].pic=mingxingdianpu[i].tupian;
+        data[i].title=mingxingdianpu[i].text+mingxingdianpu[i].num;
+    }
+    // console.log(data);
+    var slider = Zepto.ui.slider('#slider', {
+        autoPlay:false,
+        showArr:false,
+        viewNum:3,
+        content:data
+       
+    });
+   
+    // actionMingXingDianPu();
 }
 
 function makeYouHuiShangPin(youhuishangpin) {
     var strHtml = '<div class="twocolom">';
-    $.each(youhuishangpin, function(i) {
+    jQuery.each(youhuishangpin, function(i) {
         if (i % 2 == 0) {
             strHtml = strHtml + '<div class="colom"><div class="colomitem">' + '<div class="tupian"><img src=' + this.tupian + '></div><div class="content"><p class="decoration">' +
                 this.decoration + '</p><p class="money">' + this.money + '</p></div></div>';
@@ -131,31 +164,31 @@ function makeYouHuiShangPin(youhuishangpin) {
                 this.decoration + '</p><p class="money">' + this.money + '</p></div></div></div>';
         }
     });
-    $('div.youhuishangpin .body').html(strHtml);
+    jQuery('div.youhuishangpin .body').html(strHtml);
 }
 
 function makeYouHui(tupian, text, num) {
     var optionHtml = '<div class="item"><div class="tupian"><img src=' + tupian + '></div><div class="content"><p class="decoration">' +
         text + '</p><p class="reference">免费领取</p><p class="num">已领' + num + '张</p></div></div>';
 
-    $('div.youhuiquan .body').append(optionHtml);
+    jQuery('div.youhuiquan .body').append(optionHtml);
 
 
 }
 //滚动 加手动显示广告
 function actionHuoDong() {
-    var $huodong = $('div.guanggao div.huodong');
-    var ul = $('div.guanggao div.huodong ul');
-    var $num = $huodong.find('.num span');
-    var oneWidth = $huodong.find('ul li').eq(0).width();
+    var jQueryhuodong = jQuery('div.guanggao div.huodong');
+    var ul = jQuery('div.guanggao div.huodong ul');
+    var jQuerynum = jQueryhuodong.find('.num span');
+    var oneWidth = jQueryhuodong.find('ul li').eq(0).width();
     var timer = null;
     var iNow = 0;
     //delegate mingcheng shijian huo on shijain mingcheng chuli bangding
-    $('div.guanggao').delegate("span", "click", function() {
+    jQuery('div.guanggao').delegate("span", "click", function() {
         // alert("fa");
-        $(this).addClass('active')
+        jQuery(this).addClass('active')
             .siblings().removeClass('active');
-        iNow = $(this).index();
+        iNow = jQuery(this).index();
         // console.log(iNow);
 
         ul.animate({
@@ -168,98 +201,97 @@ function actionHuoDong() {
     function autoplay() {
         timer = setInterval(function() {
             iNow++;
-            if (iNow > $num.length - 1) {
+            if (iNow > jQuerynum.length - 1) {
                 iNow = 0;
             }
-            $num.eq(iNow).trigger('click');
+            jQuerynum.eq(iNow).trigger('click');
         }, 2500)
     }
     autoplay();
     //触摸悬停
-    $('div.guanggao').delegate("ul", "touchstart", function(e) {
+    jQuery('div.guanggao').delegate("ul", "touchstart", function(e) {
         //防止页面整个左右摆动
         e.preventDefault();
         clearInterval(timer);
     });
-    $('div.guanggao').delegate("ul", "touchend", function(e) {
+    jQuery('div.guanggao').delegate("ul", "touchend", function(e) {
         autoplay();
         //防止页面整个左右摆动
         e.preventDefault();
     });
     //拖拽切换图片
-    $(".huodong").hover(function() {
-        $("#btn_prev,#btn_next").fadeIn()
+    jQuery(".huodong").hover(function() {
+        jQuery("#btn_prev,#btn_next").fadeIn()
     }, function() {
-        $("#btn_prev,#btn_next").fadeOut()
+        jQuery("#btn_prev,#btn_next").fadeOut()
     });
 
-    $dragBln = false;
+    jQuerydragBln = false;
 
-    $(".huodong ").touchSlider({
+    jQuery(".huodong ").touchSlider({
         flexible: true,
         speed: 200,
-        btn_prev: $("#btn_prev"),
-        btn_next: $("#btn_next"),
-        paging: $(".num span"),
+        btn_prev: jQuery("#btn_prev"),
+        btn_next: jQuery("#btn_next"),
+        paging: jQuery(".num span"),
         counter: function(e) {
-            $(".num span").removeClass("active").eq(e.current - 1).addClass("active");
+            jQuery(".num span").removeClass("active").eq(e.current - 1).addClass("active");
         }
     });
 
-    $(".huodong .tupian").bind("dragstart", function() {
-        $dragBln = true;
+    jQuery(".huodong .tupian").bind("dragstart", function() {
+        jQuerydragBln = true;
     });
 
-    $(".huodong .tupian ").click(function() {
-        if ($dragBln) {
+    jQuery(".huodong .tupian ").click(function() {
+        if (jQuerydragBln) {
             return false;
         }
     });
 }
 
 function actionMingXingDianPu() {
-    var oneWidth = $('.row').width();
+    var oneWidth = jQuery('.row').width();
     var timer = null;
-    var $row=$('.mingxingdianpu .row');
-    var length=$row.length;
+    var jQueryrow=jQuery('.mingxingdianpu .row');
+    var length=jQueryrow.length;
     // console.log(length);
     // console.log(oneWidth);
     var pos = 0;
     
-    //  $('div.mingxingdianpu').delegate(".row", "click",function() {
-    // 	// if ($dragBln) {
+    //  jQuery('div.mingxingdianpu').delegate(".row", "click",function() {
+    // 	// if (jQuerydragBln) {
     //  //        return false;
     //  //    }
-    //     pos = $(this).index();
+    //     pos = jQuery(this).index();
     //     console.log(pos);
     //     if(pos==length-1){
     //     	pos=0;
     //     }
-    //     $('div.container').animate({
+    //     jQuery('div.container').animate({
     //         "left": -oneWidth *pos
     //     });
-    //     console.log($('div.container').css("left"));
+    //     console.log(jQuery('div.container').css("left"));
     // });
-      $dragBln = false;
+      jQuerydragBln = false;
       //触摸悬停
-    $('div.mingxingdianpu').delegate(".container", "touchstart", function(e) {
+    jQuery('div.mingxingdianpu').delegate(".container", "touchstart", function(e) {
         //防止页面整个左右摆动
         e.preventDefault();
     });
     
-    // $(".mingxingdianpu").hover(function() {
-    //     $("#btn_prev,#btn_next").fadeIn()
+    // jQuery(".mingxingdianpu").hover(function() {
+    //     jQuery("#btn_prev,#btn_next").fadeIn()
     // }, function() {
-    //     $("#btn_prev,#btn_next").fadeOut()
+    //     jQuery("#btn_prev,#btn_next").fadeOut()
     // });
    
-    $(".mingxingdianpu ").touchSlider({
-        // flexible: false,
-        // speed: 200,
-        // btn_prev: $("#btn_prev"),
-        // btn_next: $("#btn_next"),
+    // jQuery(".mingxingdianpu ").touchSlider({
+    //     // flexible: false,
+    //     // speed: 200,
+    //     // btn_prev: jQuery("#btn_prev"),
+    //     // btn_next: jQuery("#btn_next"),
        
-    });
+    // });
 
-   
 }
